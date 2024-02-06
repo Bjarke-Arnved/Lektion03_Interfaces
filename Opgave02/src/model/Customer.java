09d0cf644730ce466562b6a3fe022ae9b9b390e4
Bjarke <eaa23bjar@students.eaaa.dk>
2024-02-06 11:53:05 +0100

    Added the Customer class and the compareTo method

---
+++ Opgave02/src/model/Customer.java
@@ -0,0 +1,61 @@
+package model.Customer;
+
+public class Customer implements Comparable<Customer> {
+    private String name;
+    private String sirName;
+    private int age;
+    public Customer() {
+
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSirName() {
+        return sirName;
+    }
+    public void setSirName(String sirName) {
+        this.sirName = sirName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int compareTo(Customer otherCustomer) {
+        int result = 0;
+        // compares sirnames
+        // if they are the same age is compared
+        if(sirName.compareTo(otherCustomer.sirName) == 0) {
+            if(name.compareTo(otherCustomer.name) == 0) {
+                // if sirname, name and age is the same it is the same person
+                if(age == otherCustomer.age) {
+                    result = 0;
+                }
+                else if (age < otherCustomer.age) {
+                    result = -1;
+                }
+                else {
+                    result = 1;
+                }
+            }
+            else if(name.compareTo(otherCustomer.name) < 0) {
+                result = -1;
+            }
+            else {
+                result = 1;
+            }
+        }
+        else if(sirName.compareTo(otherCustomer.sirName) < 0) {
+            result = -1;
+        }
+        else {
+            result = 1;
+        }
+        return result;
+    }
+}
